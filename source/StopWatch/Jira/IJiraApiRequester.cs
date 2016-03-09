9eb94de22bc1706a38554c7b3de4382589427ca3
Carsten Gehling <carsten@sarum.dk>
2016-03-09 07:19:15 +0100

    Refactoring JiraClient to split responsibilities

---
+++ source/StopWatch/Jira/IJiraApiRequester.cs
@@ -0,0 +1,15 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    interface IJiraApiRequester
+    {
+        public T DoAuthenticatedRequest<T>(IRestRequest request);
+
+    }
+}
--- source/StopWatchTest/JiraClientTest.cs
+++ source/StopWatchTest/JiraClientTest.cs
@@ -13,7 +13,6 @@
         private Mock<IRestRequestFactory> requestFactoryMock;
         private Mock<IRestClient> clientMock;
         private Mock<IRestRequest> requestMock;
-        private Mock<IRestResponse> responseMock;
 
         private JiraClient jiraClient;
 
@@ -24,60 +23,96 @@
             requestFactoryMock = new Mock<IRestRequestFactory>();
             clientMock = new Mock<IRestClient>();
             requestMock = new Mock<IRestRequest>();
-            responseMock = new Mock<IRestResponse>();
+            //responseMock = new Mock<IRestResponse>();
 
             clientFactoryMock.Setup(c => c.Create(It.IsAny<string>(), It.IsAny<bool>())).Returns(clientMock.Object);
             requestFactoryMock.Setup(r => r.Create(It.IsAny<string>(), It.IsAny<Method>())).Returns(requestMock.Object);
-            clientMock.Setup(c => c.Execute(It.IsAny<IRestRequest>())).Returns(responseMock.Object);
+
+            /*
+            clientMock.Setup(c => c.Execute(It.IsAny<IRestRequest>())).Returns(new RestResponse()
+            {
+                StatusCode = HttpStatusCode.OK
+            });
+            */
+
+
 
             jiraClient = new JiraClient(clientFactoryMock.Object, requestFactoryMock.Object);
             jiraClient.BaseUrl = "http://api.example.com";
         }
 
-
-        //clientMock.Setup(c => c.Execute<Issue>(It.IsAny<IRestRequest>())).Returns(responseMock.Object);
         [Test, Description("Authenticate returns true on successful authentication")]
         public void Authenticate_OnSuccess_It_Returns_True()
         {
-            responseMock.Setup(c => c.StatusCode).Returns(HttpStatusCode.OK);
+            clientMock.Setup(c => c.Execute(It.IsAny<IRestRequest>())).Returns(new RestResponse()
+            {
+                StatusCode = HttpStatusCode.OK
+            });
             Assert.That(jiraClient.Authenticate("myuser", "mypassword"), Is.True);
         }
 
         [Test, Description("Authenticate returns false on unsuccessful authentication")]
         public void Authenticate_OnFailure_It_Returns_False()
         {
-            responseMock.Setup(c => c.StatusCode).Returns(HttpStatusCode.Unauthorized);
+            clientMock.Setup(c => c.Execute(It.IsAny<IRestRequest>())).Returns(new RestResponse()
+            {
+                StatusCode = HttpStatusCode.Unauthorized
+            });
             Assert.That(jiraClient.Authenticate("myuser", "mypassword"), Is.False);
         }
 
         [Test, Description("ValidateSession: On success it sets SessionValid and returns true")]
         public void ValidateSession_OnSuccess_It_Sets_SessionValid_And_Returns_True()
         {
-            responseMock.Setup(c => c.StatusCode).Returns(HttpStatusCode.OK);
+            //clientMock.Setup(c => c.Execute<object>(It.IsAny<IRestRequest>())).Returns(new RestResponse<object>()
+            //{
+            //    StatusCode = HttpStatusCode.OK
+            //});
+            clientMock.Setup(c => c.Execute(It.IsAny<IRestRequest>())).Returns(new RestResponse()
+            {
+                StatusCode = HttpStatusCode.OK
+            });
             Assert.That(jiraClient.ValidateSession(), Is.True);
             Assert.That(jiraClient.SessionValid, Is.True);
         }
 
         [Test, Description("ValidateSession: On failure it resets SessionValid and returns false")]
-        public void ValidateSession_OnSuccess_It_Resets_SessionValid_And_Returns_False()
+        public void ValidateSession_OnFailure_It_Resets_SessionValid_And_Returns_False()
         {
-            responseMock.Setup(c => c.StatusCode).Returns(HttpStatusCode.Unauthorized);
+            clientMock.Setup(c => c.Execute(It.IsAny<IRestRequest>())).Returns(new RestResponse()
+            {
+                StatusCode = HttpStatusCode.OK
+            });
+            //clientMock.Setup(c => c.Execute<object>(It.IsAny<IRestRequest>())).Returns(new RestResponse<object>()
+            //{
+            //    StatusCode = HttpStatusCode.Unauthorized
+            //});
             Assert.That(jiraClient.ValidateSession(), Is.False);
             Assert.That(jiraClient.SessionValid, Is.False);
         }
 
-        [Test, Description("ValidateSession: On OK, it will not try to ReAuthenticate")]
-        public void ValidateSession_OnOK_It_Will_Not_Try_To_ReAuhthenticate()
+
+        protected T <T>(IRestRequest request)
+
+
+        [Test, Description("DoAuthenticatedRequest: On OK, it will not try to ReAuthenticate")]
+        public void DoAuthenticatedRequest_OnOK_It_Will_Not_Try_To_ReAuhthenticate()
         {
-            responseMock.Setup(c => c.StatusCode).Returns(HttpStatusCode.OK);
-            jiraClient.ValidateSession();
+            clientMock.Setup(c => c.Execute<object>(It.IsAny<IRestRequest>())).Returns(new RestResponse<object>()
+            {
+                StatusCode = HttpStatusCode.OK
+            });
+            jiraClient.();
             requestFactoryMock.Verify(m => m.Create("/rest/auth/1/session", Method.POST), Times.Never);
         }
 
-        [Test, Description("ValidateSession: On unauthorized, it tries to ReAuthenticate")]
-        public void ValidateSession_OnUnauthorized_It_Tries_To_ReAuhthenticate()
+        [Test, Description("DoAuthenticatedRequest: On unauthorized, it tries to ReAuthenticate")]
+        public void DoAuthenticatedRequest_OnUnauthorized_It_Tries_To_ReAuhthenticate()
         {
-            responseMock.Setup(c => c.StatusCode).Returns(HttpStatusCode.Unauthorized);
+            clientMock.Setup(c => c.Execute<object>(It.IsAny<IRestRequest>())).Returns(new RestResponse<object>()
+            {
+                StatusCode = HttpStatusCode.Unauthorized
+            });
             jiraClient.ValidateSession();
             requestFactoryMock.Verify(m => m.Create("/rest/auth/1/session", Method.POST));
         }
