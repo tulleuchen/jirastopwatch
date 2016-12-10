f084e424445ecd39f0cb54c036905098cd1ee58b
Carsten Gehling <cg@spectra-systems.com>
2016-12-10 10:06:35 +0100

    Reorganize files into folders

---
+++ source/StopWatch/UI/SettingsForm.Designer.cs
@@ -0,0 +1,340 @@
+﻿/**************************************************************************
+Copyright 2016 Carsten Gehling
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**************************************************************************/
+namespace StopWatch
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblJiraBaseUrl = new System.Windows.Forms.Label();
+            this.tbJiraBaseUrl = new System.Windows.Forms.TextBox();
+            this.cbAlwaysOnTop = new System.Windows.Forms.CheckBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblSaveTimerState = new System.Windows.Forms.Label();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.cbMinimizeToTray = new System.Windows.Forms.CheckBox();
+            this.cbSaveTimerState = new System.Windows.Forms.ComboBox();
+            this.lblPauseOnSessionLock = new System.Windows.Forms.Label();
+            this.cbPauseOnSessionLock = new System.Windows.Forms.ComboBox();
+            this.splitter3 = new System.Windows.Forms.Label();
+            this.splitter2 = new System.Windows.Forms.Label();
+            this.splitter1 = new System.Windows.Forms.Label();
+            this.cbAllowMultipleTimers = new System.Windows.Forms.CheckBox();
+            this.cbPostWorklogComment = new System.Windows.Forms.ComboBox();
+            this.lblPostWorklogComment = new System.Windows.Forms.Label();
+            this.cbAllowManualEstimateAdjustments = new System.Windows.Forms.CheckBox();
+            this.lblDisplayOptions = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStartTransitions = new System.Windows.Forms.Label();
+            this.tbStartTransitions = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblJiraBaseUrl
+            // 
+            this.lblJiraBaseUrl.AutoSize = true;
+            this.lblJiraBaseUrl.Location = new System.Drawing.Point(9, 7);
+            this.lblJiraBaseUrl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblJiraBaseUrl.Name = "lblJiraBaseUrl";
+            this.lblJiraBaseUrl.Size = new System.Drawing.Size(70, 13);
+            this.lblJiraBaseUrl.TabIndex = 0;
+            this.lblJiraBaseUrl.Text = "JIRA base url";
+            // 
+            // tbJiraBaseUrl
+            // 
+            this.tbJiraBaseUrl.Location = new System.Drawing.Point(122, 5);
+            this.tbJiraBaseUrl.Margin = new System.Windows.Forms.Padding(2);
+            this.tbJiraBaseUrl.Name = "tbJiraBaseUrl";
+            this.tbJiraBaseUrl.Size = new System.Drawing.Size(259, 20);
+            this.tbJiraBaseUrl.TabIndex = 1;
+            // 
+            // cbAlwaysOnTop
+            // 
+            this.cbAlwaysOnTop.AutoSize = true;
+            this.cbAlwaysOnTop.Location = new System.Drawing.Point(122, 46);
+            this.cbAlwaysOnTop.Margin = new System.Windows.Forms.Padding(2);
+            this.cbAlwaysOnTop.Name = "cbAlwaysOnTop";
+            this.cbAlwaysOnTop.Size = new System.Drawing.Size(158, 17);
+            this.cbAlwaysOnTop.TabIndex = 4;
+            this.cbAlwaysOnTop.Text = "Always keep window on top";
+            this.cbAlwaysOnTop.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.Location = new System.Drawing.Point(263, 363);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(56, 22);
+            this.btnOk.TabIndex = 17;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(324, 363);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(56, 22);
+            this.btnCancel.TabIndex = 18;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // lblSaveTimerState
+            // 
+            this.lblSaveTimerState.Location = new System.Drawing.Point(9, 105);
+            this.lblSaveTimerState.Name = "lblSaveTimerState";
+            this.lblSaveTimerState.Size = new System.Drawing.Size(98, 38);
+            this.lblSaveTimerState.TabIndex = 7;
+            this.lblSaveTimerState.Text = "Save timer states on program exit";
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(11, 363);
+            this.btnAbout.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(56, 22);
+            this.btnAbout.TabIndex = 16;
+            this.btnAbout.Text = "About...";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // cbMinimizeToTray
+            // 
+            this.cbMinimizeToTray.AutoSize = true;
+            this.cbMinimizeToTray.Location = new System.Drawing.Point(122, 67);
+            this.cbMinimizeToTray.Margin = new System.Windows.Forms.Padding(2);
+            this.cbMinimizeToTray.Name = "cbMinimizeToTray";
+            this.cbMinimizeToTray.Size = new System.Drawing.Size(98, 17);
+            this.cbMinimizeToTray.TabIndex = 5;
+            this.cbMinimizeToTray.Text = "Minimize to tray";
+            this.cbMinimizeToTray.UseVisualStyleBackColor = true;
+            // 
+            // cbSaveTimerState
+            // 
+            this.cbSaveTimerState.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSaveTimerState.FormattingEnabled = true;
+            this.cbSaveTimerState.Location = new System.Drawing.Point(122, 107);
+            this.cbSaveTimerState.Name = "cbSaveTimerState";
+            this.cbSaveTimerState.Size = new System.Drawing.Size(258, 21);
+            this.cbSaveTimerState.TabIndex = 8;
+            // 
+            // lblPauseOnSessionLock
+            // 
+            this.lblPauseOnSessionLock.Location = new System.Drawing.Point(9, 143);
+            this.lblPauseOnSessionLock.Name = "lblPauseOnSessionLock";
+            this.lblPauseOnSessionLock.Size = new System.Drawing.Size(98, 38);
+            this.lblPauseOnSessionLock.TabIndex = 9;
+            this.lblPauseOnSessionLock.Text = "Pause timer on session lock";
+            // 
+            // cbPauseOnSessionLock
+            // 
+            this.cbPauseOnSessionLock.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPauseOnSessionLock.FormattingEnabled = true;
+            this.cbPauseOnSessionLock.Location = new System.Drawing.Point(122, 145);
+            this.cbPauseOnSessionLock.Name = "cbPauseOnSessionLock";
+            this.cbPauseOnSessionLock.Size = new System.Drawing.Size(176, 21);
+            this.cbPauseOnSessionLock.TabIndex = 10;
+            // 
+            // splitter3
+            // 
+            this.splitter3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitter3.Location = new System.Drawing.Point(12, 352);
+            this.splitter3.Name = "splitter3";
+            this.splitter3.Size = new System.Drawing.Size(370, 2);
+            this.splitter3.TabIndex = 15;
+            // 
+            // splitter2
+            // 
+            this.splitter2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitter2.Location = new System.Drawing.Point(12, 93);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(370, 2);
+            this.splitter2.TabIndex = 6;
+            // 
+            // splitter1
+            // 
+            this.splitter1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitter1.Location = new System.Drawing.Point(12, 37);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(370, 2);
+            this.splitter1.TabIndex = 2;
+            // 
+            // cbAllowMultipleTimers
+            // 
+            this.cbAllowMultipleTimers.AutoSize = true;
+            this.cbAllowMultipleTimers.Location = new System.Drawing.Point(122, 183);
+            this.cbAllowMultipleTimers.Margin = new System.Windows.Forms.Padding(2);
+            this.cbAllowMultipleTimers.Name = "cbAllowMultipleTimers";
+            this.cbAllowMultipleTimers.Size = new System.Drawing.Size(228, 17);
+            this.cbAllowMultipleTimers.TabIndex = 11;
+            this.cbAllowMultipleTimers.Text = "Allow running multiple timers simultaneously";
+            this.cbAllowMultipleTimers.UseVisualStyleBackColor = true;
+            // 
+            // cbPostWorklogComment
+            // 
+            this.cbPostWorklogComment.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPostWorklogComment.FormattingEnabled = true;
+            this.cbPostWorklogComment.Location = new System.Drawing.Point(122, 212);
+            this.cbPostWorklogComment.Name = "cbPostWorklogComment";
+            this.cbPostWorklogComment.Size = new System.Drawing.Size(198, 21);
+            this.cbPostWorklogComment.TabIndex = 13;
+            // 
+            // lblPostWorklogComment
+            // 
+            this.lblPostWorklogComment.Location = new System.Drawing.Point(9, 210);
+            this.lblPostWorklogComment.Name = "lblPostWorklogComment";
+            this.lblPostWorklogComment.Size = new System.Drawing.Size(98, 38);
+            this.lblPostWorklogComment.TabIndex = 12;
+            this.lblPostWorklogComment.Text = "How to post the worklog comment";
+            // 
+            // cbAllowManualEstimateAdjustments
+            // 
+            this.cbAllowManualEstimateAdjustments.AutoSize = true;
+            this.cbAllowManualEstimateAdjustments.Location = new System.Drawing.Point(122, 244);
+            this.cbAllowManualEstimateAdjustments.Margin = new System.Windows.Forms.Padding(2);
+            this.cbAllowManualEstimateAdjustments.Name = "cbAllowManualEstimateAdjustments";
+            this.cbAllowManualEstimateAdjustments.Size = new System.Drawing.Size(200, 17);
+            this.cbAllowManualEstimateAdjustments.TabIndex = 14;
+            this.cbAllowManualEstimateAdjustments.Text = "Allow control over remaining estimate";
+            this.cbAllowManualEstimateAdjustments.UseVisualStyleBackColor = true;
+            // 
+            // lblDisplayOptions
+            // 
+            this.lblDisplayOptions.Location = new System.Drawing.Point(9, 48);
+            this.lblDisplayOptions.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDisplayOptions.Name = "lblDisplayOptions";
+            this.lblDisplayOptions.Size = new System.Drawing.Size(107, 34);
+            this.lblDisplayOptions.TabIndex = 3;
+            this.lblDisplayOptions.Text = "Display Options";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(11, 271);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 2);
+            this.label1.TabIndex = 19;
+            // 
+            // lblStartTransitions
+            // 
+            this.lblStartTransitions.Location = new System.Drawing.Point(9, 285);
+            this.lblStartTransitions.Name = "lblStartTransitions";
+            this.lblStartTransitions.Size = new System.Drawing.Size(107, 52);
+            this.lblStartTransitions.TabIndex = 20;
+            this.lblStartTransitions.Text = "Possible state changes when pressing play (seperate by newline)";
+            // 
+            // tbStartTransitions
+            // 
+            this.tbStartTransitions.AcceptsReturn = true;
+            this.tbStartTransitions.Location = new System.Drawing.Point(122, 285);
+            this.tbStartTransitions.Multiline = true;
+            this.tbStartTransitions.Name = "tbStartTransitions";
+            this.tbStartTransitions.Size = new System.Drawing.Size(200, 52);
+            this.tbStartTransitions.TabIndex = 21;
+            // 
+            // SettingsForm
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(397, 393);
+            this.Controls.Add(this.tbStartTransitions);
+            this.Controls.Add(this.lblStartTransitions);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbAllowManualEstimateAdjustments);
+            this.Controls.Add(this.cbPostWorklogComment);
+            this.Controls.Add(this.lblPostWorklogComment);
+            this.Controls.Add(this.cbAllowMultipleTimers);
+            this.Controls.Add(this.cbPauseOnSessionLock);
+            this.Controls.Add(this.splitter3);
+            this.Controls.Add(this.cbSaveTimerState);
+            this.Controls.Add(this.splitter2);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.lblPauseOnSessionLock);
+            this.Controls.Add(this.cbMinimizeToTray);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.lblSaveTimerState);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.cbAlwaysOnTop);
+            this.Controls.Add(this.lblDisplayOptions);
+            this.Controls.Add(this.tbJiraBaseUrl);
+            this.Controls.Add(this.lblJiraBaseUrl);
+            this.Icon = global::StopWatch.Properties.Resources.stopwatchicon;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "SettingsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "StopWatch Settings";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SettingsForm_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblJiraBaseUrl;
+        private System.Windows.Forms.TextBox tbJiraBaseUrl;
+        private System.Windows.Forms.CheckBox cbAlwaysOnTop;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblSaveTimerState;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.CheckBox cbMinimizeToTray;
+        private System.Windows.Forms.ComboBox cbSaveTimerState;
+        private System.Windows.Forms.ComboBox cbPauseOnSessionLock;
+        private System.Windows.Forms.Label lblPauseOnSessionLock;
+        private System.Windows.Forms.Label splitter3;
+        private System.Windows.Forms.Label splitter2;
+        private System.Windows.Forms.Label splitter1;
+        private System.Windows.Forms.CheckBox cbAllowMultipleTimers;
+        private System.Windows.Forms.ComboBox cbPostWorklogComment;
+        private System.Windows.Forms.Label lblPostWorklogComment;
+        private System.Windows.Forms.CheckBox cbAllowManualEstimateAdjustments;
+        private System.Windows.Forms.Label lblDisplayOptions;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStartTransitions;
+        private System.Windows.Forms.TextBox tbStartTransitions;
+    }
+}
---
+++ source/StopWatch/UI/SettingsForm.cs
@@ -0,0 +1,110 @@
+﻿/**************************************************************************
+Copyright 2016 Carsten Gehling
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**************************************************************************/
+using System.Windows.Forms;
+
+namespace StopWatch
+{
+
+
+    internal partial class SettingsForm : Form
+    {
+        #region public members
+        public Settings settings { get; private set; }
+        #endregion
+
+
+        #region public methods
+        public SettingsForm(Settings settings)
+        {
+            this.settings = settings;
+
+            InitializeComponent();
+
+            // Mono for MacOSX and Linux do not implement the notifyIcon
+            // so ignore this feature if we are not running on Windows
+            cbMinimizeToTray.Visible = CrossPlatformHelpers.IsWindowsEnvironment();
+
+            tbJiraBaseUrl.Text = this.settings.JiraBaseUrl;
+            cbAlwaysOnTop.Checked = this.settings.AlwaysOnTop;
+            cbMinimizeToTray.Checked = this.settings.MinimizeToTray;
+            cbAllowMultipleTimers.Checked = this.settings.AllowMultipleTimers;
+            cbAllowManualEstimateAdjustments.Checked = this.settings.AllowManualEstimateAdjustments;
+
+            cbSaveTimerState.DisplayMember = "Text";
+            cbSaveTimerState.ValueMember = "Value";
+            cbSaveTimerState.DataSource = new[]
+            {
+                new { Text = "Reset all timers on exit", Value = SaveTimerSetting.NoSave },
+                new { Text = "Save current timetracking, pause active timer", Value = SaveTimerSetting.SavePause },
+                new { Text = "Save current timetracking, active timer continues", Value = SaveTimerSetting.SaveRunActive }
+            };
+            cbSaveTimerState.SelectedValue = this.settings.SaveTimerState;
+
+            cbPauseOnSessionLock.DisplayMember = "Text";
+            cbPauseOnSessionLock.ValueMember = "Value";
+            cbPauseOnSessionLock.DataSource = new[]
+            {
+                new { Text = "No pause", Value = PauseAndResumeSetting.NoPause },
+                new { Text = "Pause active timer", Value = PauseAndResumeSetting.Pause },
+                new { Text = "Pause and resume on unlock", Value = PauseAndResumeSetting.PauseAndResume }
+            };
+            cbPauseOnSessionLock.SelectedValue = this.settings.PauseOnSessionLock;
+
+            cbPostWorklogComment.DisplayMember = "Text";
+            cbPostWorklogComment.ValueMember = "Value";
+            cbPostWorklogComment.DataSource = new[]
+            {
+                new { Text = "Post only as part of worklog", Value = WorklogCommentSetting.WorklogOnly },
+                new { Text = "Post only as a comment", Value = WorklogCommentSetting.CommentOnly },
+                new { Text = "Post as both worklog and comment", Value = WorklogCommentSetting.WorklogAndComment }
+            };
+            cbPostWorklogComment.SelectedValue = this.settings.PostWorklogComment;
+
+            tbStartTransitions.Text = this.settings.StartTransitions;
+        }
+        #endregion
+
+
+        #region private eventhandlers
+        private void SettingsForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (DialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                this.settings.JiraBaseUrl = tbJiraBaseUrl.Text;
+                this.settings.AlwaysOnTop = cbAlwaysOnTop.Checked;
+                this.settings.MinimizeToTray = cbMinimizeToTray.Checked;
+                this.settings.AllowMultipleTimers = cbAllowMultipleTimers.Checked;
+                this.settings.AllowManualEstimateAdjustments = cbAllowManualEstimateAdjustments.Checked;
+
+                this.settings.SaveTimerState = (SaveTimerSetting)cbSaveTimerState.SelectedValue;
+                this.settings.PauseOnSessionLock = (PauseAndResumeSetting)cbPauseOnSessionLock.SelectedValue;
+                this.settings.PostWorklogComment = (WorklogCommentSetting)cbPostWorklogComment.SelectedValue;
+
+                this.settings.StartTransitions = tbStartTransitions.Text;
+            }
+        }
+
+
+        private void btnAbout_Click(object sender, System.EventArgs e)
+        {
+            using (var aboutForm = new AboutForm())
+            {
+                aboutForm.ShowDialog();
+            }
+        }
+        #endregion
+    }
+}
