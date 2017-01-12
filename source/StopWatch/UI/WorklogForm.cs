3ecb4acedd36b5b66fbe910d7369f4937d7aad98
Carsten Gehling <cg@spectra-systems.com>
2017-01-12 19:15:30 +0100

    Fix typo in worklog form

--- source/StopWatch/UI/WorklogForm.cs
+++ source/StopWatch/UI/WorklogForm.cs
@@ -304,7 +304,7 @@ namespace StopWatch
         {
             if (string.IsNullOrWhiteSpace(RemainingEstimate))
             {
-                rdEstimateAdjustLeave.Text = "&Leave Unchanged bar";
+                rdEstimateAdjustLeave.Text = "&Leave Unchanged";
             }
             else
             {
